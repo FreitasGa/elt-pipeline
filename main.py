7ce2da54d1fcbfbfd0721c6054ff1b921fe3a7b3
Gabriel Freitas <freitasga.01@gmail.com>
2024-09-20 22:42:23 -0300

    add imc categories and queries

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json
+import math
 import sqlite3
 
 import requests
@@ -15,7 +15,8 @@ def scaffold_database(connection: sqlite3.Connection) -> None:
             name TEXT,
             height REAL,
             weight REAL,
-            imc REAL
+            imc REAL,
+            imc_category TEXT
         )
         """
     )
@@ -55,6 +56,11 @@ def transform_data(raw_data: list) -> pandas.DataFrame | None:
     df['weight'] = df['weight'] / 10
     df['height'] = df['height'] / 10
     df['imc'] = round(df['weight'] / (df['height'] ** 2), 2)
+    df['imc_category'] = pandas.cut(
+        df['imc'],
+        bins=[0, 18.5, 24.9, 29.9, 34.9, 39.9, math.inf],
+        labels=['Underweight', 'Normal weight', 'Overweight', 'Obesity I', 'Obesity II', 'Obesity III']
+    )
 
     return df
 
@@ -65,14 +71,73 @@ def load_data(data_frame: pandas.DataFrame, connection: sqlite3.Connection) -> N
     for item in data_frame.itertuples():
         cursor.execute(
             """
-            INSERT INTO pokemon (id, name, height, weight, imc)
-            VALUES (?, ?, ?, ?, ?)
-            ON CONFLICT (id) DO UPDATE SET name = excluded.name, height = excluded.height, weight = excluded.weight, imc = excluded.imc
+            INSERT INTO pokemon (id, name, height, weight, imc, imc_category)
+            VALUES (?, ?, ?, ?, ?, ?)
+            ON CONFLICT (id) DO UPDATE SET name = excluded.name, height = excluded.height, weight = excluded.weight, imc = excluded.imc, imc_category = excluded.imc_category
             """,
-            (item.id, item.name, item.height, item.weight, item.imc)
+            (item.id, item.name, item.height, item.weight, item.imc, item.imc_category)
         )
 
     cursor.close()
+
+
+def query_heaviest_pokemon_by_imc_category(connection: sqlite3.Connection) -> list:
+    cursor = connection.cursor()
+
+    cursor.execute(
+        """
+        SELECT DISTINCT  p.*
+        FROM pokemon AS p
+        INNER JOIN (
+            SELECT imc_category, MAX(imc) as max_imc
+            FROM pokemon
+            GROUP BY imc_category
+        ) AS max_p on p.imc_category = max_p.imc_category AND p.imc = max_p.max_imc
+        """
+    )
+
+    data = cursor.fetchall()
+    cursor.close()
+
+    return data
+
+
+def query_weakest_pokemon_by_imc_category(connection: sqlite3.Connection) -> list:
+    cursor = connection.cursor()
+
+    cursor.execute(
+        """
+        SELECT p.*
+        FROM pokemon AS p
+        INNER JOIN (
+            SELECT imc_category, MIN(imc) as min_imc
+            FROM pokemon
+            GROUP BY imc_category
+        ) AS min_p on p.imc_category = min_p.imc_category AND p.imc = min_p.min_imc
+        """
+    )
+
+    data = cursor.fetchall()
+    cursor.close()
+
+    return data
+
+
+def query_count_by_imc_category(connection: sqlite3.Connection) -> list:
+    cursor = connection.cursor()
+
+    cursor.execute(
+        """
+        SELECT imc_category, COUNT(*) as count 
+        FROM pokemon
+        GROUP BY imc_category
+        """
+    )
+
+    data = cursor.fetchall()
+    cursor.close()
+
+    return data
 
 
 def main() -> None:
@@ -92,6 +157,14 @@ def main() -> None:
         return
 
     load_data(data_frame, connection)
+
+    count_by_imc_category = query_count_by_imc_category(connection)
+    heaviest_pokemon_by_imc_category = query_heaviest_pokemon_by_imc_category(connection)
+    weakest_pokemon_by_imc_category = query_weakest_pokemon_by_imc_category(connection)
+
+    print("Pokemon by IMC category:", count_by_imc_category)
+    print("Heaviest Pokemon by IMC category:", heaviest_pokemon_by_imc_category)
+    print("Weakest Pokemon by IMC category:", weakest_pokemon_by_imc_category)
 
     connection.commit()
     connection.close()
