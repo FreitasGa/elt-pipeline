c5b4623a97114ed64fd1dc9ffb7964ed7436b1d9
Gabriel Freitas <freitasga.01@gmail.com>
2024-09-20 21:53:59 -0300

    done with poke api

--- main.py
+++ main.py
@@ -10,44 +10,72 @@ def scaffold_database(connection: sqlite3.Connection) -> None:
 
     cursor.execute(
         """
-        
+        CREATE TABLE IF NOT EXISTS pokemon (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            height REAL,
+            weight REAL,
+            imc REAL
+        )
         """
     )
 
-    connection.commit()
+    cursor.close()
 
 
 def fetch_data() -> list | None:
-    url = "https://pokeapi.co/api/v2/pokemon?limit=10"
-    response = requests.get(url)
+    url = "https://beta.pokeapi.co/graphql/v1beta"
+    body = """ 
+    query { 
+      result: pokemon_v2_pokemon(limit: 151) {
+        id
+        name
+        height
+        weight
+      }
+    } 
+    """
+    response = requests.post(url, json={"query": body})
 
     if response.status_code != 200:
         return None
 
-    data = json.loads(response.json())
+    raw_data = response.json()
 
-    return data.results
+    return raw_data['data']['result']
 
 
 def transform_data(raw_data: list) -> pandas.DataFrame | None:
     df = pandas.DataFrame(raw_data)
 
-    return None
+    if df.empty:
+        return None
+
+    df = df[['id', 'name', 'height', 'weight']]
+    df['weight'] = df['weight'] / 10
+    df['height'] = df['height'] / 10
+    df['imc'] = round(df['weight'] / (df['height'] ** 2), 2)
+
+    return df
 
 
 def load_data(data_frame: pandas.DataFrame, connection: sqlite3.Connection) -> None:
     cursor = connection.cursor()
 
-    cursor.execute(
-        """
-        
-        """
-    )
+    for item in data_frame.itertuples():
+        cursor.execute(
+            """
+            INSERT INTO pokemon (id, name, height, weight, imc)
+            VALUES (?, ?, ?, ?, ?)
+            ON CONFLICT (id) DO UPDATE SET name = excluded.name, height = excluded.height, weight = excluded.weight, imc = excluded.imc
+            """,
+            (item.id, item.name, item.height, item.weight, item.imc)
+        )
 
-    connection.commit()
+    cursor.close()
 
 
-def main():
+def main() -> None:
     connection = sqlite3.connect("local.db")
     scaffold_database(connection)
 
@@ -64,6 +92,8 @@ def main():
         return
 
     load_data(data_frame, connection)
+
+    connection.commit()
     connection.close()
 
 
